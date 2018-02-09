8f5525e5446a323a28c1e32ef22b53e8a2ef70e0
ziyanw1 <ziyanw1@andrew.cmu.edu>
2018-02-08 22:22:59 -0500

    add render results on v1

--- data/render_scripts/render_depth_pair_lambert_func_continuous_persp_template.py
+++ data/render_scripts/render_depth_pair_lambert_func_continuous_persp_template.py
@@ -228,6 +228,9 @@ else:
     shape_file = "/dataset/ShapeNetCore.v2/{0}/{1}/models/model_normalized.obj".format(CATEGORY,MODEL)
     lights_num = 35
     scene.world.light_settings.ao_factor = 0.15
+shape_file = "/dataset/ShapeNetCore.v1/{0}/{1}/model.obj".format(CATEGORY,MODEL)
+lights_num = 20
+scene.world.light_settings.ao_factor = 0.05
 bpy.ops.import_scene.obj(filepath=shape_file) 
 
 print("================= OBJ file loaded sucessfully!=================")
