6073af5c029522c9ee6cfda883446a2a59079dd5
ziyanw1 <ziyanw1@andrew.cmu.edu>
2018-02-14 20:14:43 -0500

    experiment script

---
+++ train_rgb2dep.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+python ./train_ae_rgb2d_persp_lmdb.py --LOG_DIR=log --CHECKPOINT_DIR=ckpt --task_name=rgb2depth_0213 --force_delete --voxel_resolution=32 \
+    --batch_size=16 --vis_every_step=1 --data_path=data/lmdb --data_file=rgb2depth_single_0212 \
+    --save_every_step=10000 --loss_name=sketch_loss --opt_step_name=opt_step 
+
+#python ./train_ae_rgb2d_nopara_lmdb.py --LOG_DIR=log --CHECKPOINT_DIR=ckpt --task_name=rgb2depth_0213 --force_delete --voxel_resolution=32 \
+#    --batch_size=16 --vis_every_step=1 --data_path=data/lmdb --data_file=rgb2depth_single_0212 \
+#    --save_every_step=10000 --loss_name=sketch_loss --opt_step_name=opt_step 
