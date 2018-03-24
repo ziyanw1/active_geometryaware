69615c1291e623562eac5d448200fb6b0178c635
ricson <ricsonc@andrew.cmu.edu>
2018-03-23 22:08:16 -0400

    add script for debugging

---
+++ debug.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python train_active_mvnet.py --is_training=True --task_name='debug_' \
+     --max_episode_length=4 --reward_type='IG' --learning_rate=1e-3 --burn_in_length=1 \
+     --test_every_step=1 --save_every_step=10 --max_iter=10000 --batch_size=4 --voxel_resolution=64 \
+     --resolution=128 --category='03001627' --mem_length=100 --burn_in_iter=1
