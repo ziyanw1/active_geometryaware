fixed issue with rotation and axis direction

env_data/replay_memory.py
@@ -124,7 +124,7 @@ class ReplayMemory():
             data = np.transpose(data, (0,2,1))
         return data
 
-    def read_vox(self, vox_name, transpose = True): 
+    def read_vox(self, vox_name, transpose = False):
         vox_model = self.read_bv(vox_name, transpose) 
         vox_factor = self.voxel_resolution * 1.0 / 128
         #vox_model_zoom = ndimg.zoom(vox_model, vox_factor, order=0) # nearest neighbor interpolation

models/active_mvnet.py
@@ -83,7 +83,8 @@ class ActiveMVnet(object):
 
         def rotate_voxels(vox, az0, el0):
             vox = tf.expand_dims(vox, axis = 4)
-            R = other.voxel.get_transform_matrix_tf(az0, el0)
+            #negative sign is important -- although i'm not sure how it works
+            R = other.voxel.get_transform_matrix_tf(-az0, el0)
             return other.voxel.rotate_voxel(other.voxel.transformer_preprocess(vox), R)
 
         def tile_voxels(x):

util_unproj.py
@@ -50,5 +50,4 @@ class Unproject_tools:
             for (depth_, mask_, additional_, rot_) in zip(depths, masks, additionals, rots)
         ]
 
-        return tf.stack(unprojects, axis = 1)
-        
+        return tf.stack(unprojects, axis = 1) #4 x 4 x V x V x V x 7